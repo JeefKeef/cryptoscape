eb54c9cdd7cf4240f51a761cdef1ab98170a2dd4
Jeff <nguyen.jeffreyson@gmail.com>
2022-01-20 14:04:36 -0800

    Implemented crypto news filter
    - Implemented material ui Autocomplete for crypto news filter

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,30 +1,73 @@
+import moment from "moment";
 import React, { useState, useEffect } from "react";
 import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+import { useGetCryptosQuery } from "../services/cryptoApi";
+import { Input } from "@material-ui/core";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+
+const newsDemoImage =
+  "http://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg";
+
 const News = ({ simplified }) => {
+  const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
+  const { data: cryptoList } = useGetCryptosQuery(100);
+  const { data: cryptoNews, isFetching } = useGetCryptoNewsQuery({
+    newsCategory,
+    count: simplified ? 6 : 12,
+  });
 
-    //const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
-    //const { data } = useGetCryptosQuery(100);
-    //const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 6 : 12 });
-
-  //if(isFetching) return 'Loading...';
-
-  // const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
-  // const { data } = useGetCryptosQuery(100);
-  // const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 10 : 100 });
-  
-  // //if(!cryptoNews?.value) return 'Loading...';
-  
-  // console.log(" start");
-  // console.log(cryptoNews);
-  // console.log("news end");
-
-
-  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 1 : 3 });
+  if (isFetching) return "Loading...";
 
   console.log(cryptoNews);
 
   return (
-    <div>News</div>);
+    <div>
+      <div>
+        {!simplified && (
+          <div>
+            <Autocomplete
+              id="search-news"
+              freeSolo
+              options={cryptoList?.data?.coins.map((coin) => coin.name)}
+              renderInput={(params) => (
+                <TextField {...params} label="Cryptocurrencies" />
+              )}
+              onChange={(currency) => setNewsCategory(currency.target.innerText)}
+            />
+          </div>
+        )}
+      </div>
+      {cryptoNews?.value.map((news) => (
+        <div>
+          <a href={news?.url} target="_blank" rel="noreferrer">
+            <div>
+              <h1>{news?.name}</h1>
+              <img
+                src={news?.image?.thumbnail?.contentUrl || newsDemoImage}
+                alt="news"
+              />
+            </div>
+            <p>
+              {news?.description > 100
+                ? `${news.description.substring(0, 100)}...`
+                : news.description}
+            </p>
+            <div>
+              <div>
+                <img
+                  src={news?.provider[0]?.image?.thumbnail?.contentUrl}
+                  alt=""
+                />
+                <h6>{news?.provider[0]?.name}</h6>
+              </div>
+              <h6>{moment(news?.datePublished).startOf("ss").fromNow()}</h6>
+            </div>
+          </a>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default News;
